9406960a3e8f31d6233a1c18bf97e644622723db
carlitosharvey-testpro <carlitos@testpro.io>
2026-09-19 10:29:25 +0800

    test 1

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -23,7 +23,7 @@ public class LoginTests extends BaseTest {
 
         // TODO (for students): Review the configuration as part of HW15
         
-        String url = "https://testpro.io/";
+        String url = "https://app.testpro.io/";
         //Test
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
